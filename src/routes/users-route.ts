4c4aeb4949391f66f879cd5866a9493ec359c32f
Diego Loiola <diego.alves_1996@hotmail.com>
2021-11-01 11:05:15 -0300

    Instalando http-status-codes para melhorar a visualização de status

--- src/routes/users-route.ts
+++ src/routes/users-route.ts
@@ -1,15 +1,16 @@
 import { NextFunction, Response, Request, Router } from "express";
+import { StatusCodes } from "http-status-codes";
 
 const usersRoute = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
     const users = [{ username: 'Diego'}];
-    res.status(200).send(users)
+    res.status(StatusCodes.OK).send(users)
 })
 
 usersRoute.get('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
-    res.status(200).send({uuid});
+    res.status(StatusCodes.OK).send({uuid});
 })
 
 export default usersRoute;
