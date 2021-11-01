f1884f4e589175d1139d7a773c470006021141d1
Diego Loiola <diego.alves_1996@hotmail.com>
2021-11-01 11:43:16 -0300

    Inserindo o método POST na rota de usuários

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,15 @@ import usersRoute from './routes/users-route';
 
 const app = express();
 
-app.use(usersRoute);
+// Configurações da Aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
+// Configurações de Rotas
+app.use(usersRoute);
 app.get('/status', (req: Request, res: Response, next: NextFunction)=>{
     res.status(200).send({ foo: 'Mudança'})
 })
 
+// Inicialização do servidor
 app.listen(4000, ()=> console.log('Executando...'));
--- src/routes/users-route.ts
+++ src/routes/users-route.ts
@@ -13,4 +13,9 @@ usersRoute.get('/users/:uuid', (req: Request<{uuid: string}>, res: Response, nex
     res.status(StatusCodes.OK).send({uuid});
 })
 
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+    const newUser = req.body;
+    res.status(StatusCodes.CREATED).send(newUser)
+})
+
 export default usersRoute;
