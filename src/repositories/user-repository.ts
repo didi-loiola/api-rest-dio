9987f43ca865e92ac3aec15b12ad4615da8beeaf
Diego Loiola <diego.alves_1996@hotmail.com>
2021-11-02 11:22:15 -0300

    Adicionando usuário e colocando variável de ambiente dentro do salto de hash

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -1,5 +1,6 @@
 import db from "../db";
 import User from "../models/user-model";
+require('dotenv').config();
 
 class UserRepository {
     async findAllUsers(): Promise<User[]> {
@@ -17,6 +18,21 @@ class UserRepository {
         const [ user ] = rows;
         
         return user;
+    }
+
+    async create(user: User): Promise<string> {
+        const script = `INSERT INTO application_user (
+            username,
+            password
+        )
+        VALUES ($1, crypt($2,$3))
+        RETURNING uuid;
+        `
+        const values = [user.username, user.password, process.env.SECRET];
+
+        const { rows } = await db.query<{ uuid: string }>(script, values);
+        const [ newUser ] = rows;
+        return newUser.uuid;
     }
 }
 
