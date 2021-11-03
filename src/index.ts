13bc903a78f867e5af6d3d6f4dfd41e23f443067
Diego Loiola <diego.alves_1996@hotmail.com>
2021-11-03 11:40:26 -0300

    Criando middleware de erro genérico

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response, NextFunction} from 'express';
 import usersRoute from './routes/users-route';
 import statusRoute from './routes/status-route';
+import errorHandler from './middlewares/error-handler-middleware';
 
 const app = express();
 
@@ -11,6 +12,9 @@ app.use(express.urlencoded({ extended: true }))
 // Configurações de Rotas
 app.use(usersRoute);
 app.use(statusRoute);
+
+// Configuração dos Handlers de Erro
+app.use(errorHandler);
 
 // Inicialização do servidor
 app.listen(4000, ()=> console.log('Executando...'));
